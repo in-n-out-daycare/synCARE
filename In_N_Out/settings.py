530914ba405c70d9268704eb45bf1e0f29240673
bjgribb <bjgribb@gmail.com>
2019-04-15 14:48:46 -0400

    removed staticdirs from settings

--- In_N_Out/settings.py
+++ In_N_Out/settings.py
@@ -16,7 +16,6 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
