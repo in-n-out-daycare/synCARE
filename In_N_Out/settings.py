9fdd1b79e624b4435df0b22dd788d8d925ee7436
bjgribb <bjgribb@gmail.com>
2019-04-15 15:00:49 -0400

    updated settings.py

--- In_N_Out/settings.py
+++ In_N_Out/settings.py
@@ -16,10 +16,11 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
