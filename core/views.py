1e6c93f8f5e43455dabf31d7d27236a2c2e33d6e
Taylor Cooke <taylorcooke@gmail.com>
2019-04-15 17:01:16 -0400

    working on views and urls

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Child, Activity
+from django.views import generic
+
 
 # Create your views here.
+
+class ChildListView(generic.ListView):
+    model = Child;
+
+class ActivityListView(generic.ListView):
+    model = Activity;
+
+def eat(request):
+    
