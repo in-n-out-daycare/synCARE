3bc61aa80a9997545a9f6114f3e289cbbe9fb50d
bjgribb <bjgribb@gmail.com>
2019-04-16 11:32:30 -0400

    Model update for default user model

--- core/models.py
+++ core/models.py
@@ -1,41 +1,28 @@
 import uuid
 from django.db import models 
+from django.contrib.auth.models import User
 
-
-class DaycareAdmin(models.Model):
-    name = models.CharField(max_length=50, help_text="Last name, First name")
-    role = models.CharField(max_length=50, help_text="your role in the facility")
- 
 
 class Classroom(models.Model):
     
-    INFANT = 1
-    TODDLER = 2
-    PRE_K = 3
-    
-    CATEGORY_CHOICES = (
-        (INFANT, 'Infant'),
-        (TODDLER, 'Toddler'),
-        (PRE_K, 'Pre_k'),
-    )
-    category = models.PositiveIntegerField(choices=CATEGORY_CHOICES, null=False)
-    
+    classroom = models.CharField(max_length=254, null=False)
+    caregiver = models.ForeignKey(to=User, related_name="classrooms", on_delete=models.CASCADE)
 
-class Caregiver(models.Model):
-    name = models.CharField(max_length=50, help_text="Last name, First name")
-    role = models.CharField(max_length=50, help_text="your role in the facility")
-    classroom = models.ForeignKey(to=Classroom, related_name="caregivers", on_delete=models.CASCADE)
-    
+    def __str__(self):
+        return self.classroom
+
 
 class Guardian(models.Model):
     name = models.CharField(max_length=50, help_text="Last name, First name")
     phone_number = models.CharField(max_length=12, null=False)
     email = models.EmailField(max_length=254, null=False)
+
+    def __str__(self):
+        return self.name
     
 
 class Child(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    full_name = models.CharField(max_length=254)
     age = models.CharField(max_length=2, null=False)
     child_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     classroom = models.ForeignKey(to=Classroom, related_name="children", on_delete=models.CASCADE)
@@ -43,12 +30,17 @@ class Child(models.Model):
     child_pic = models.ImageField(null=True)
     allergy = models.TextField(max_length=2000, null=True)
 
+    def __str__(self):
+        return self.full_name
 
 
 class Visit(models.Model):
     check_in = models.DateTimeField('Check in', auto_now_add=True)
     check_out = models.DateTimeField('Check out', default=False)
     child = models.ForeignKey(to=Child,related_name="visits", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.check_in
 
 
 class Activity(models.Model):
@@ -70,9 +62,9 @@ class Activity(models.Model):
     )
     activity_type = models.CharField(max_length=2, choices=ACTIVITY_TYPES, null=False)
     visit = models.ForeignKey(to=Visit, related_name="activities", on_delete=models.CASCADE)
-    caregiver = models.ForeignKey(to=Caregiver, related_name="activities", on_delete=models.CASCADE)
+    child = models.ForeignKey(to=Child, related_name="activities", on_delete=models.CASCADE)
     start_time = models.DateTimeField('Start time', auto_now_add=True)
     end_time = models.DateTimeField('End time', default=False)
 
-
-
+    def __str__(self):
+        return activity_type
