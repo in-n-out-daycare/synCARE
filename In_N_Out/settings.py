8107aa39f021baf304ef2a89b01ac55f85cf6c71
bjgribb <bjgribb@gmail.com>
2019-04-16 10:29:24 -0400

    created separate heroku settings file

--- In_N_Out/settings.py
+++ In_N_Out/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,7 +141,5 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-django_heroku.settings(locals())
 
 LOGIN_REDIRECT_URL = '/home/'
