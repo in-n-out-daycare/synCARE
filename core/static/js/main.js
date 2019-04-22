5c8e7e1ec128589e3b82f43e318989a0512c0e7c
Taylor Cooke <taylorcooke@gmail.com>
2019-04-22 16:24:57 -0400

    button created for bottle and displaying options when clicked

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -49,3 +49,24 @@ function validateFoodForm () {
 function submitFoodForm () {
   if (validateFoodForm()) document.getElementById('food_form').submit()
 }
+
+
+function myFunction() {
+    
+    document.getElementById("foodChoices").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    
+    if (!event.target.matches('.dropButton')) {
+        
+        var dropdowns = document.getElementsByClassName("dropdownContent");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
